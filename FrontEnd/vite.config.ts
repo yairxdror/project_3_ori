f1b92b6c9a948a4d65f8ab93f7b754e51ba33be1
yair dror <yairx66@gmail.com>
2025-12-28 16:34:25 +0200

    icon

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: "/project_3_ori/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
 });
